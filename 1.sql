9ccb99976af04535fb06a7aad5a0cda96e0b5913
srinu1622 <r95310737@gmail.com>
2025-03-12 07:08:58 +0530

    day 3

---
+++ 1.sql
@@ -0,0 +1,35 @@
+--select sysdate from dual
+--select systimestamp from dual
+
+
+--select current_date from dual
+--select current_timestamp from dual
+ 
+--select dbtimezone from dual
+
+--select sysdate,sysdate+4 nextweek from dual
+--select sysdate,sysdate-4 previousweek from dual 
+
+--select * from hr.employees
+--select hire_date,hire_date+5 nextweek from hr.employees
+--select hire_date,hire_date-2 previousweek from hr.employees
+--select hire_date,hire_date+3 nextmonths from hr.employees
+ --select systimestamp,systimestamp+interval '2' hour from dual
+-- select systimestamp,systimestamp+interval'3' minute from dual
+--select systimestamp,systimestamp+interval '6' second from dual
+--select systimestamp,systimestamp+interval '2' day+interval '4' hour from dual
+
+
+--select * from hr.employees
+--select hire_date from hr.employees
+--select hire_date,extract(year from hire_date) year,extract(month from hire_date)
+--month,extract(day from hire_date)day from hr.employees
+
+--select add_months(sysdate,6) from dual
+--select hire_date,last_day(hire_date)from hr.employees
+---select next_day(sysdate,'monday') from dual
+
+
+
+
+

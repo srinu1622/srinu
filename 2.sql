65c3c7671ef0998f80d0b85bcdfe84feca5eda0e
srinu1622 <r95310737@gmail.com>
2025-03-12 07:09:19 +0530

    Add files via upload

---
+++ 2.sql
@@ -0,0 +1,34 @@
+--select abs(months_between(hire_date,sysdate))/12 as yearsofexp from hr.employees
+
+--t_char
+--to_date
+--to_timestamp
+
+--select to_char(sysdate,'day')from dual
+
+
+--null functions
+
+--select null from dual
+--select nvl(null,'data engineering') from dual
+--select nvl(null,'2000')from dual
+
+
+--nvl2 (expression,value_if_not_vale,value_if_value)
+--select nvl2(null,'has value','has no value')from dual
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
